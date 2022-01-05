909e649ad376e9863e5f1c7bba14387cf18722ef
AmyCalleja <91883080+amy-calleja@users.noreply.github.com>
2022-01-05 11:18:24 +0100

    Added Phonetics and audio player

---
+++ src/KeywordPhonetic.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactAudioPlayer from "react-audio-player";
+
+export default function KeywordPhonetic(props) {
+  console.log(props.phonetic);
+  return (
+    <div className="KeywordPhonetic">
+      /{props.phonetic.text}
+      <div className="audioPlayer">
+        <ReactAudioPlayer
+          src={props.phonetic.audio}
+          autoPlay={false}
+          controls
+        />
+      </div>
+    </div>
+  );
+}
--- src/SearchResults.js
+++ src/SearchResults.js
@@ -1,19 +1,25 @@
 import React from "react";
 import KeywordMeaning from "./KeywordMeaning";
+import KeywordPhonetic from "./KeywordPhonetic";
 
 export default function SearchResults(props) {
   if (props.results) {
     return (
       <div className="SearchResults">
         <h2>{props.results.word}</h2>
+        {props.results.phonetics.map(function (phonetic, index) {
+          return (
+            <div key={index}>
+              <KeywordPhonetic phonetic={phonetic} />
+            </div>
+          );
+        })}
         {props.results.meanings.map(function (meaning, index) {
           return (
             <div key={index}>
               <KeywordMeaning meaning={meaning} />
             </div>
           );
-
-          //meaning.definitions[0].definition;
         })}
       </div>
     );
