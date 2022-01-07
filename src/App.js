078e24907567e28a16ba4ad7aba5ceda8729c6e8
AmyCalleja <91883080+amy-calleja@users.noreply.github.com>
2022-01-07 17:52:22 +0100

    Added logo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Dictionary from "./Dictionary";
 import Footer from "./Footer";
+import logo from "./logo.png";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <section>
+          <img src={logo} alt="search" />
           <h1>Dictionary App</h1>
           <Dictionary defaultKeyword="sunset" />
         </section>
